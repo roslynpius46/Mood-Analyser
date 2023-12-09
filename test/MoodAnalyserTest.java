05a9c6e57085ce10985a82ae561cacafaa5321b5
roslynpius <roslyn.pius@gmail.com>
2023-12-09 21:02:31 +0530

    Added: UC 3- Handle Exception if user provides empty message

--- test/MoodAnalyserTest.java
+++ test/MoodAnalyserTest.java
@@ -71,6 +71,10 @@ public class MoodAnalyserTest {
         }
     }
 
+    /**
+     *
+     * @desc Test case for empty message
+     */
     @Test
     public void testAnalyseMoodEmptyMessageShouldThrowMoodAnalysisException() {
         try {
