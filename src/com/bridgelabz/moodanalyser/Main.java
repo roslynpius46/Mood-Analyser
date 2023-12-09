70f23a665f77440e451d14c5fc114c6817fad6aa
roslynpius <roslyn.pius@gmail.com>
2023-12-09 18:51:37 +0530

    Initial commit

---
+++ src/com/bridgelabz/moodanalyser/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.moodanalyser;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to mood analyser problem");
+        
+    }
+}
